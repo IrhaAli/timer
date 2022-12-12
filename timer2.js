f486920af7ca548e7cc8ca48f817610ef8873caa
Irha Ali <irhaali_97@hotmail.ca>
2022-12-12 16:24:55 -0500

    Use stdin to complete interactive timer.

--- timer2.js
+++ timer2.js
@@ -1,20 +1,32 @@
-const prompt = require("prompt-sync")({ sigint: true });
-const interactiveTimer = function() {
-  let condition = false;
-  while (!condition) {
-    let timer = prompt('Timer set for (in seconds): ');
-    if (Number(timer) < 0) {
-      console.log("Time travel is not yet possible. A positive number is needed.");
-    } else if (Number.isNaN(Number(timer))) {
-      console.log("Time is a quantifiable object. Hence, a number must be provided.");
-    } else {
-      setTimeout(() => {
-        process.stdout.write('\x07');
-      }, Number(timer));
-    }
+// Another method: `put it inside of event listener to listen to keyboard input (look at snake exc).
+const interactiveTimer = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+// Handles the following user input: ctrl + c, b, 1, ... , 9 seconds
+const handleUserInput = function (timer) {
+  let numTimer = Number(timer);
+  if (timer === '\u0003') {
+    console.log('Thanks for using me, ciao!')
+    process.exit();
+  } else if (numTimer < 0) {
+    console.log("Time travel is not yet possible. A positive number is needed.");
+  } else if (Number.isNaN(numTimer)) {
+    (timer === 'b') ? process.stdout.write('\x07') : console.log("Time is a quantifiable object. Hence, a number must be provided.");
+  }
+  else if (1 <= numTimer && numTimer <= 9) {
+    console.log(`Setting timer for ${timer} seconds.`);
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, numTimer * 1000);
+  } else {
+    console.log(`Timer must be between 1 and 9 seconds`);
   }
 };
 
 interactiveTimer();
-// how to catch ctrl + c
-// then you have to manually exit of the process (check how to exit a process)
