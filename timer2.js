651a407649a7f21f836906567106f75ca13b11ee
Irha Ali <irhaali_97@hotmail.ca>
2022-12-15 16:07:11 -0500

    Eslint this

--- timer2.js
+++ timer2.js
@@ -1,5 +1,5 @@
 // Another method: `put it inside of event listener to listen to keyboard input (look at snake exc).
-const interactiveTimer = function () {
+const interactiveTimer = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,7 +9,7 @@ const interactiveTimer = function () {
 };
 
 // Handles the following user input: ctrl + c, b, 1, ... , 9 seconds
-const handleUserInput = function (timer) {
+const handleUserInput = function(timer) {
   let numTimer = Number(timer);
   if (timer === '\u0003') {
     console.log('Thanks for using me, ciao!');
@@ -18,8 +18,7 @@ const handleUserInput = function (timer) {
     console.log("Time travel is not yet possible. A positive number is needed.");
   } else if (Number.isNaN(numTimer)) {
     (timer === 'b') ? process.stdout.write('\x07') : console.log("Time is a quantifiable object. Hence, a number must be provided.");
-  }
-  else if (1 <= numTimer && numTimer <= 9) {
+  } else if (1 <= numTimer && numTimer <= 9) {
     console.log(`Setting timer for ${timer} seconds.`);
     setTimeout(() => {
       process.stdout.write('\x07');
